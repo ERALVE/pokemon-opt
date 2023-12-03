30f4ca28a5e69eeecab1bb2d297d264e1c07dcd3
Thania Peralta <thania.peralta@gmail.com>
2023-12-03 12:20:35 -0500

    inicializacion de mi projecto

--- owner/views.py
+++ owner/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def owner_list(request):
+
+    data_context= {
+        'nombre':  'Samanta Peralta',
+        'edad': 20,
+        'dni': "1234567",
+        'pais': "Perú",
+        'vigente': False,
+    }
+
+    return render(request, 'owner_list.html', context={'data': data_context})
